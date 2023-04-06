ba488f6a5b4f4d9bc990c877cf4f1cc07072632f
DenisBoytsov41 <goodpromised41@gmail.com>
2023-04-06 15:28:30 +0300

    Осуществляю работу

--- src/dictionary1/DictionaryMain.java
+++ src/dictionary1/DictionaryMain.java
@@ -6,9 +6,10 @@ public interface DictionaryMain
 {
     static void complete(String filename){} // Запуск программы
     static void readFile (String filename){} // Чтение из файла
-    static void writeFile (String filename){} // Запись в файл
+    static void writeFile (String filename,HashMap map){} // Запись в файл
     static void searchingKey (String filename, HashMap map){} // Поиск по ключу
     static void removeKey (String filename, HashMap map){} // Удалить по ключу
     static void addForHashMap(String filename, HashMap hashMap){}; // Добавление в Map
+    //static HashMap chooseStr(String line, HashMap hmap){} // Формат поиска
 
 }
