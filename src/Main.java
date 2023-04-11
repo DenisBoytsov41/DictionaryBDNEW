f3ca6adc4294631b5e6eac170749de36dfdac18f
DenisBoytsov41 <goodpromised41@gmail.com>
2023-04-11 22:51:51 +0300

    Может быть готово

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,21 @@
-import dictionary1.DictionaryFirstTapy;
-import dictionary1.DictionarySecondTapy;
+import dictionary1.DictionaryMain;
 
-import java.util.HashMap;
 import java.util.Scanner;
 
-public class Main {
+public class Main{
     public static void main(String[] args)
     {
+        //C:\\Users\\Денис\\Desktop\\text_txt.txt
         try {
             System.out.print("Введите путь к файлу: ");
             Scanner filen = new Scanner(System.in);
             String filename = filen.nextLine();
-            System.out.println("Выберите словарь:");
-            System.out.println("1 - Английский гипотетический-Цифры");
-            System.out.println("2 - Цифры-Английский гипотетический");
-            HashMap map = new HashMap();
-            Scanner choose = new Scanner(System.in);
-            int number = choose.nextInt();
-            switch (number) {
-                case 1:
-                    DictionaryFirstTapy dft = new DictionaryFirstTapy(filename);
-                    break;
-                case 2:
-                    DictionarySecondTapy dst = new DictionarySecondTapy(filename);
-                    break;
-                default:
-                    break;
-            }
+            DictionaryMain.workMain(filename);
         }
         catch (Exception e)
         {
             System.out.print("Вы ввели некорректный путь к файлу!");
         }
     }
+
 }
